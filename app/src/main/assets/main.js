0937d08bc961f518bfc801d689d7cb22ad304218
asafonov <me@asafonov.org>
2024-04-11 14:26:35 +0000

    minor fix

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -725,7 +725,7 @@ class UpdaterView {
   }
 }
 window.asafonov = {}
-window.asafonov.version = '0.2'
+window.asafonov.version = '0.3'
 window.asafonov.messageBus = new MessageBus()
 window.asafonov.timer = new Timer(40)
 window.asafonov.events = {
@@ -740,8 +740,6 @@ window.onerror = (msg, url, line) => {
   alert(`${msg} on line ${line}`)
 }
 document.addEventListener("DOMContentLoaded", function (event) {
-  const updaterView = new UpdaterView('https://raw.githubusercontent.com/asafonov/accelerace/master/VERSION.txt', 'https://github.com/asafonov/accelerace.apk/releases/download/{VERSION}/app-release.apk')
-  updaterView.showUpdateDialogIfNeeded()
   asafonov.timer.setInterval(80)
   const gameView = new GameView(5500)
 })
