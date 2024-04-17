fe3d92841bea113966a353384f70f253e492c625
asafonov <me@asafonov.org>
2024-04-17 13:51:20 +0000

    sensors initial implementation

--- app/src/main/java/org/asafonov/accelerace/MainActivity.java
+++ app/src/main/java/org/asafonov/accelerace/MainActivity.java
@@ -5,16 +5,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.content.Context;
 
 public class MainActivity extends Activity {
 
     private WebView mWebView;
+    SensorManager sensorManager;
+    Sensor sensor;
 
     @Override
     @SuppressLint("SetJavaScriptEnabled")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(Context.SYSTEM_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         mWebView = findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -33,4 +42,25 @@ public class MainActivity extends Activity {
             super.onBackPressed();
         }
     }
+
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(gyroListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(gyroListener);
+    }
+
+    public SensorEventListener gyroListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+        }
+    };
 }
