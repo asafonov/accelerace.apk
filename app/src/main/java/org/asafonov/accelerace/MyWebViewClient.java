8a3e5b87400c0664c7e54bf1174cb005d5760c44
dimitris <jimitrakis11@gmail.com>
2026-06-05 16:33:09 +0200

    Catch ActivityNotFoundException when opening external links

--- app/src/main/java/org/asafonov/accelerace/MyWebViewClient.java
+++ app/src/main/java/org/asafonov/accelerace/MyWebViewClient.java
@@ -1,10 +1,12 @@
 package org.asafonov.accelerace;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebResourceRequest;
+import android.widget.Toast;
 
 class MyWebViewClient extends WebViewClient {
 
@@ -15,7 +17,11 @@ class MyWebViewClient extends WebViewClient {
         if (url.substring(0, 5).equals("https")) {
           Uri webpage = Uri.parse(url);
           Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-          view.getContext().startActivity(intent);
+          try {
+            view.getContext().startActivity(intent);
+          } catch (ActivityNotFoundException e) {
+            Toast.makeText(view.getContext(), "No app found to open this link", Toast.LENGTH_SHORT).show();
+          }
           return true;
         }
 
